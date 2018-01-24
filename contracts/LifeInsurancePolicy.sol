c580a6e9f55465183816082d32786fe4c9828502
agilajah <febiagil20@gmail.com>
2018-01-24 17:55:01 +0700

    update code

--- contracts/LifeInsurancePolicy.sol
+++ contracts/LifeInsurancePolicy.sol
@@ -38,5 +38,71 @@ library SafeMath {
 }
 
 contract LifeInsurancePolicy is PolicyInvestable { 
+    using SafeMath for uint256;
+    // Investment data 
+    mapping (address => uint) public investors;
+    uint public totalInvestorsCount;
+    uint public totalInvestedAmount;
+
+    uint public totalInsurers;
+    uint public totalClaimsPaid;
+    
+    uint128 public investmentsLimit;
+    uint32 public investmentsDeadlineTimeStamp;
+    
+    uint8 constant DECIMAL_PRECISION = 8;
+    uint24 constant ALLOWED_RETURN_INTERVAL_SEC = 24 * 60 * 60; // each 24 hours
+
+    mapping (address => DividendLine[]) private payedDividends;
+    uint public payedDividendsAmount;
+
+    // Insurance data
+    uint public policiesLimit;
+    mapping (address => PolicyData) insurancePolicies;
+    mapping (string => mapping(string => uint) ) insuranceParameters;
+    uint public basePremium;
+    uint public maxPayout;
+    uint loading;
+    uint public writtenPremiumAmount;
+    uint32 public lastPolicyDate;
+
+    event Insured(string deviceName, uint insurancePrice);
+    event Claimed(uint payout); 
+    event DividendsPayed(uint date, uint payout); 
+
+    struct DividendLine{
+        uint amount;
+        uint32 transferDate;
+    }
+
+    struct PolicyData {
+        DeviceData device;
+        uint endDateTimestamp;
+        uint nextPaymentTimestamp;
+        uint monthlyPayment;
+        uint maxPayout;
+        uint totalPrice;
+        string region;
+        bool claimed;
+        bool confirmed;
+    }
+
+    struct InsuredData {
+        string userId;
+        string name;
+    }
+
+    function LifeInsurancePolicy() payable {
+        // Initial funds
+        investors[msg.sender] = investors[msg.sender] + msg.value;
+        totalInvestorsCount++;
+        totalInvestedAmount = totalInvestedAmount + msg.value;
+        Invested(msg.value);
+
+        setInitialInsuranceParameters();
+    }
+
+
+
 
 }
